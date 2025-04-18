c215e431db4d8aadc2b08495f280de0a82b1c01e
Lenka Koubková <lvseteckova@gmail.com>
2025-04-18 10:04:58 +0200

    přidán index html a script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+const kniha = {
+  nazev:"Osudy dobrého vojáka Švejka",
+  autor:{
+    jmeno:"Jaroslav",
+    prijmeni:"Hašek",
+    vek:45,   //nemusí být uvozovky, protože jsou to čísla
+  },
+  pocetStran:380,
+  ilustrator:"Josef Lada",
+  dilyPocet:5,
+herec: {
+  filmPrvniDil:"Osudy dobrého vojáka Švejka",
+  hlavniPostava: {
+    jmeno:"Rudolf",
+    prijmeni:"Hrusinsky",
+  }
+},
+};
